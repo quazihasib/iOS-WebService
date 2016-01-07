70afff35a460c2a829a601ef7efb021cd7d3c62e
Quazi Ridwan Hasib <quazihasib@192-168-1-3.tpgi.com.au>
2016-01-07 12:19:22 +1100

    Initial Commit

---
+++ WebService1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WebService1
+//
+//  Created by Quazi Ridwan Hasib on 7/01/2016.
+//  Copyright © 2016 Quazi Ridwan Hasib. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ WebService1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WebService1
+//
+//  Created by Quazi Ridwan Hasib on 7/01/2016.
+//  Copyright © 2016 Quazi Ridwan Hasib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
