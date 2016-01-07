da76c0f62e9021af2512a8f805512b943f948871
Quazi Ridwan Hasib <quazihasib@192-168-1-3.tpgi.com.au>
2016-01-07 12:48:23 +1100

    first commit

--- WebService1/ViewController.m
+++ WebService1/ViewController.m
@@ -8,6 +8,11 @@
 
 #import "ViewController.h"
 
+
+#define kBgQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0) //1
+#define kLatestKivaLoansURL [NSURL URLWithString:@"https://api.meetup.com/2/groups?lat=51.509980&lon=-0.133700&page=20&key=1f5718c16a7fb3a5452f45193232"]
+
+
 @interface ViewController ()
 
 @end
@@ -17,11 +22,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    getData();
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+void getData()
+{
+    NSError* error;
+    NSData* data = [NSData dataWithContentsOfURL: kLatestKivaLoansURL];
+    
+    NSDictionary* json = [NSJSONSerialization JSONObjectWithData:data
+                                                         options:kNilOptions
+                                                           error:&error];
+    
+    
+    //NSArray* latestLoan = [json objectForKey:@"loans"];
+    NSLog(@"mmm:%@",json);
 }
 
 @end
